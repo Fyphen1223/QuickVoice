c7568e1de540c8826e554f23bf5f923eba7820bf
Fyphen <fyphensub@gmail.com>
2023-12-31 06:07:33 +0000

    add: add

--- index.js
+++ index.js
@@ -16,22 +16,22 @@ const RateLimit = require('express-rate-limit');
 
 const app = express();
 const sessions = session({
-	secret: config.settings.cookieSecret,
-	resave: true,
-	saveUninitialized: true,
-	name: 'session',
-	cookie: {
-		httpOnly: true,
-		secure: true,
-		maxAge: 60000,
-	},
+    secret: config.settings.cookieSecret,
+    resave: true,
+    saveUninitialized: true,
+    name: 'session',
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: 60000,
+    },
 });
 app.use(sessions);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 const limiter = RateLimit({
-	windowMs: 1 * 60 * 1000,
-	max: 100,
+    windowMs: 1 * 60 * 1000,
+    max: 100,
 });
 app.use(limiter);
 app.use(express.json());
@@ -39,76 +39,76 @@ app.set('trust proxy', 1);
 
 var server;
 if (config.settings.secure) {
-	server = https.createServer(
-		{
-			key: fs.readFileSync('./ssl/privatekey.pem'),
-			cert: fs.readFileSync('./ssl/cert.pem'),
-			ca: fs.readFileSync('./ssl/chain.pem'),
-		},
-		app
-	);
+    server = https.createServer(
+        {
+            key: fs.readFileSync('./ssl/privatekey.pem'),
+            cert: fs.readFileSync('./ssl/cert.pem'),
+            ca: fs.readFileSync('./ssl/chain.pem'),
+        },
+        app
+    );
 } else {
-	server = http.createServer(app);
+    server = http.createServer(app);
 }
 const io = new socketio.Server(server);
 io.engine.use(sessions);
 
 server.listen(config.settings.port, () => {
-	console.log(`Server started on ${config.settings.port}`);
-	return;
+    console.log(`Server started on ${config.settings.port}`);
+    return;
 });
 
 app.get('/', async function (req, res) {
-	if (!req.session.username) {
-		res.redirect('/login');
-	} else {
-		res.set('Content-Type', 'text/html');
-		res.send(fs.readFileSync('./assets/web/index.html'));
-	}
+    if (!req.session.username) {
+        res.redirect('/login');
+    } else {
+        res.set('Content-Type', 'text/html');
+        res.send(fs.readFileSync('./assets/web/index.html'));
+    }
 });
 app.get('/login', async function (req, res) {
-	res.set('Content-Type', 'text/html');
-	res.send(fs.readFileSync('./assets/web/login.html'));
+    res.set('Content-Type', 'text/html');
+    res.send(fs.readFileSync('./assets/web/login.html'));
 });
 
 app.get('/logout', async function (req, res) {
-	req.session.destroy();
-	res.redirect('/login');
+    req.session.destroy();
+    res.redirect('/login');
 });
 
 app.post('/login', (req, res) => {
-	const { username, password } = req.body;
-	if (users[username].password === password) {
-		req.session.regenerate(() => {
-			req.session.username = username;
-			res.redirect('/');
-		});
-	} else {
-		res.redirect('/login');
-	}
+    const { username, password } = req.body;
+    if (users[username].password === password) {
+        req.session.regenerate(() => {
+            req.session.username = username;
+            res.redirect('/');
+        });
+    } else {
+        res.redirect('/login');
+    }
 });
 app.get('/assets/style.css', async function (req, res) {
-	res.set('Content-Type', 'text/css');
-	res.send(fs.readFileSync('./assets/web/style.css'));
+    res.set('Content-Type', 'text/css');
+    res.send(fs.readFileSync('./assets/web/style.css'));
 });
 app.get('/assets/index.js', async function (req, res) {
-	res.set('Content-Type', 'text/javascript');
-	res.send(fs.readFileSync('./assets/web/index.js'));
+    res.set('Content-Type', 'text/javascript');
+    res.send(fs.readFileSync('./assets/web/index.js'));
 });
 
 io.on('connection', (socket) => {
-	console.log('A user connected');
-	socket.on('disconnect', () => {
-		console.log('A user disconnected');
-	});
-	socket.on('result', async (data) => {
-		const username = socket.request.session.username;
-		if (!queue.users[username]) queue.add(username, 1);
-		const res = await queue.users[username].generate(data);
-		socket.emit('result', res);
-	});
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+    socket.on('result', async (data) => {
+        const username = socket.request.session.username;
+        if (!queue.users[username]) queue.add(username, 1);
+        const res = await queue.users[username].generate(data);
+        socket.emit('result', res);
+    });
 });
 
 process.on('uncaughtException', function (err) {
-	console.log(err.stack);
+    console.log(err.stack);
 });
